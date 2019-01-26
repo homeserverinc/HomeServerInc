af4ed016b912c4400a2c7aba73e3f92f9eebfbe2
marcioelias <marcioelias@gmail.com>
2019-01-26 02:35:57 -0200

    Implemented Model and Controller methods for a basic post API call

--- app/Contractor.php
+++ app/Contractor.php
@@ -2,9 +2,37 @@
 
 namespace App;
 
+use App\Traits\Uuidable;
 use Illuminate\Database\Eloquent\Model;
 
 class Contractor extends Model
 {
-    //
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'uuid';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+    
+    protected $fillable = [
+        'name', 
+        'company', 
+        'address', 
+        'phone', 
+        'email'
+    ];
 }
--- app/Http/Controllers/ContractorsController.php
+++ app/Http/Controllers/ContractorsController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Contractor;
+use App\traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class ContractorsController extends Controller
 {
+    use ApiResponse;
     /**
      * Display a listing of the resource.
      *
@@ -81,5 +83,33 @@ class ContractorsController extends Controller
     public function destroy(Contractor $contractor)
     {
         //
+    }
+
+    public function apiStore(Contractor $contractor) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:3',
+            'company' => 'required',
+            'email' => 'required|email'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->getApiResponse($validator, 'fail', $request->all());
+        }
+       
+        try {
+            
+            DB::beginTransaction();
+            
+            $contractor = new Contractor($request->all());
+
+            $contractor->save();
+            DB::commit();
+
+            return $this->getApiResponse($contractor);
+        } catch (\Exception $e) {
+            
+            DB::rollback();
+            return $this->getApiResponse($e->getMessage(), 'error', $request->all());
+        }
     }
 }

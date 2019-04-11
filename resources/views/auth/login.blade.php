fc5edb398878fbcb6536ee78612a6749f8c5c1e0
marcioelias <marcioelias@gmail.com>
2019-04-11 11:46:18 -0300

    Change OWA tracker code.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,23 +51,25 @@ html,body{
         </div>
     </div>
 </div>
-<!-- Start Open Web Analytics Tracker -->
 <script type="text/javascript">
 //<![CDATA[
-var owa_baseUrl = 'https://owa.homeserverinc.com/';
-var owa_cmds = owa_cmds || [];
-owa_cmds.push(['setSiteId', 'c79c0258e4b5b759ccb7c4f3f3c2b738']);
-owa_cmds.push(['trackPageView']);
-owa_cmds.push(['trackClicks']);
-owa_cmds.push(['trackDomStream']);
-
-(function() {
-	var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
-	owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
-	_owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
-	var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
-}());
+var owa_baseUrl = "https://owa.homeserverinc.com";
+document.write(unescape("<script src='" + owa_baseUrl + "modules/base/js/owa.tracker-combined-min.js' type='text/javascript'> </script>"));
 //]]>
 </script>
-<!-- End Open Web Analytics Code -->
+
+<script type="text/javascript">
+//<![CDATA[
+try {
+   OWA.setSetting('baseUrl', owa_baseUrl);
+   OWATracker = new OWA.tracker();
+   OWATracker.setSiteId('c79c0258e4b5b759ccb7c4f3f3c2b738');
+   OWATracker.trackPageView();
+   OWATracker.trackClicks();
+   OWATracker.trackDomStream();
+} catch(err) {
+    console.log(err);
+}
+//]]>
+</script>
 @endsection

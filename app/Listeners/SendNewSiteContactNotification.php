396c5ef157078d246daca8268806bd4f44432707
Márcio Elias <marcioelias@gmail.com>
2019-05-09 03:32:23 -0300

    Notification of new Contacts using Telegram

--- app/Listeners/SendNewSiteContactNotification.php
+++ app/Listeners/SendNewSiteContactNotification.php
@@ -7,11 +7,12 @@ use App\Traits\TwimlClientTrait;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Traits\TelegramTrait;
 
 class SendNewSiteContactNotification
 {
 
-    use TwimlClientTrait;
+    use TwimlClientTrait, TelegramTrait;
 
     /**
      * Create the event listener.
@@ -31,11 +32,13 @@ class SendNewSiteContactNotification
      */
     public function handle(NewSiteContact $event)
     {
-        $twilio = $this->getClient()
+        /* $twilio = $this->getClient()
                         ->messages
                         ->create($event->to, [
                             'from' => $event->contact->site->phone->phone_number,
                             'body' => 'New contact from '.$event->contact->site->name.'. '.action('SiteContactsController@edit', ['uuid' => $event->contact->uuid])
-                        ]);
+                        ]); */
+
+        $this->sendMessage('[NEW-CONTACT]::['.$event->contact->site->name.']: We have received a new contact. '.action('SiteContactsController@edit', ['uuid' => $event->contact->uuid]));
     }
 }
